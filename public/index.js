318ecb46396c5ca605edc2cd9a26ad54e8033509
Tatiana Stewart <tstwrt7@gmail.com>
2024-03-18 01:34:50 -0400

    Main pages

---
+++ public/index.js
@@ -0,0 +1,6 @@
+import React from "react";
+import ReactDOM from "react-dom";
+
+import App from "../public/react/components/App.jsx";
+
+ReactDOM.render(<App />, document.getElementById("root"));
---
+++ server/_main_.py
@@ -0,0 +1,48 @@
+import random
+from pos_data import pos_system
+
+def generateOrder():
+    name_list = pos_system['names']
+    entrees_list = pos_system['menu_items']['entrees']
+    sides_list = pos_system['menu_items']['sides']
+    beverages_list = pos_system['menu_items']['beverages']
+    desserts_list = pos_system['menu_items']['desserts']
+
+    customer_name = random.choice(name_list)
+    num_guests = random.randint(1, 4)
+
+    entree_order = []
+    for _ in range(num_guests):
+       entree_item = random.choice(entrees_list)
+       entree_order.append(entree_item['item'])
+
+    side_order = []
+    for _ in range(num_guests):
+        side_item = random.choice(sides_list)
+        side_order.append(side_item['item'])
+
+    beverage_order = []
+    for _ in range(num_guests):
+        bev_item = random.choice(beverages_list)
+        beverage_order.append(bev_item['item'])
+
+    dessert_order = []
+    for _ in range(num_guests):
+        dessert_item = random.choice(desserts_list)
+        dessert_order.append(dessert_item['item'])    
+
+    full_order = {
+        'customer': customer_name,
+        'covers': num_guests,
+        'order': {
+            'entrees': entree_order,
+            'sides': side_order,
+            'beverages': beverage_order,
+            'desserts': dessert_order
+        }
+    }    
+
+    return full_order
+
+print(generateOrder())
+
