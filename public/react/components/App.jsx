032419dd1afe4cc4c8fd6acfc2f7a7d3e6ce1ed5
Tatiana <tstwrt7@gmail.com>
2024-03-18 20:46:26 -0400

    Moved fetchOrder function to GameToggle Component - fetches when game is started

--- public/react/components/App.jsx
+++ public/react/components/App.jsx
@@ -2,35 +2,19 @@ import React, { useState, useEffect } from 'react';
 import Header from './Header';
 import OrderTicket from './OrderTicket';
 import POS from './POS';
-import axios from 'axios';
-import apiURL from '../api';
 
 
 function App() {
-    const [gameIsStarted, setGameIsStarted] = useState(false)
     const [order, setOrder] = useState([]);
 
-    useEffect(() => {
-        async function fetchOrder() {
-            try {
-                const res = await axios.get(`${apiURL}/generate_order`);
-                const data = res.data;
-                console.log(data);
-                setOrder(data)
-            } catch (error) {
-                console.error('Error fetching order', error);
-            }
-        };
-        fetchOrder();    
-    }, []);
 
     return (
         <main>
-            <Header gameIsStarted={gameIsStarted}/>
+            <Header />
 
             <div className='flex'>
                 <OrderTicket order={order} />
-                <POS order={order}/>
+                <POS order={order} setOrder={setOrder}/>
             </div>
         </main>
     )
--- public/react/components/GameToggle.jsx
+++ public/react/components/GameToggle.jsx
@@ -1,23 +1,71 @@
-import React from "react";
-import Header from "./Header";
+import React, { useState, useEffect } from "react";
+import axios from "axios";
+import apiURL from "../api";
 
-function GameToggle({ gameIsStarted, setGameIsStarted }) {
+function GameToggle({ setOrder, setItems }) {
+    const [gameIsStarted, setGameIsStarted] = useState(false)
+
     function endGame() {
         // Add Functionality to show all stats and end the game
         window.alert('End Game?')
+        setGameIsStarted(false);
+        setOrder([]);
+        setItems({
+            entrees: [],
+            sides: [],
+            beverages: [],
+            desserts: []
+        })
     }
 
-    function handleButtonClick() {
-        setGameIsStarted(!gameIsStarted)
-        console.log('Game is started', gameIsStarted);
-    }
+    // Sets State to true 
+    function startGame() {
+        setGameIsStarted(true);
+        setItems({
+            entrees: [],
+            sides: [],
+            beverages: [],
+            desserts: []
+        })
+    };
+
+    // Fetches order if gameIsStarted is true upon state change
+    useEffect(() => {
+        if(gameIsStarted) {
+            fetchOrder();
+        }
+    }, [gameIsStarted]);
+
+    // Function to fetch random order - Flask API from Python function
+    async function fetchOrder() {
+        try {
+            const res = await axios.get(`${apiURL}/generate_order`);
+            const data = res.data;
+            console.log(data);
+            setOrder(data)
+        } catch (error) {
+            console.error('Error fetching order', error);
+        }
+    };     
+
 
     return (
         <>
+        {/* Renders buttons according to state */}
         {gameIsStarted ? 
-            <button id="clockout" className="btn btn-warning" onClick={endGame}>Clock Out</button>
+            <button 
+            id="clockout" 
+            className="btn btn-warning" 
+            onClick={endGame}>
+                Clock Out
+            </button>
             :
-            <button id="start-game" className="btn btn-success" onClick={handleButtonClick}>Start Game</button>
+            <button 
+            id="clockin" 
+            className="btn btn-success" 
+            onClick={startGame}>
+                Clock In
+            </button>
         }
         </>
     )
